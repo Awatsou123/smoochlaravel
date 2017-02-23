0bcb84e687a0579d23501d66f3dc713b28e0738a
Marios Paligiannis <mariospaligiannis@gmail.com>
2017-02-23 17:37:47 +0000

    OK

--- app/User.php
+++ app/User.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiabe;
+    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
